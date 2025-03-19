c6340ca5ec3da296a580e933b3b7867e48e6c8f6
Ali Emre Şişman <145591935+aliemressman@users.noreply.github.com>
2025-03-19 23:53:58 +0300

    Add files via upload

---
+++ Odevler-Ocak1/hafta1_odev.py
@@ -0,0 +1,20 @@
+# Konu: Değişkenler, veri tipleri (int, float, string, boolean), kullanıcıdan veri alma.
+
+print("Adınızı giriniz:")
+ad = input()
+
+print("Yaşınızı giriniz:")
+yas = int(input())
+
+print("Doğum yılınızı giriniz:")
+dogum_yili = int(input())
+
+print("Merhaba", ad, yas ,"Yaşındasın ve ",dogum_yili ,"Yılında doğmuşssun.")
+
+print("-------------------------------------------")
+
+print("İki sayı giriniz:")
+sayi1 = int(input())
+sayi2 = int(input())
+
+print("Toplam:", sayi1 + sayi2, "Fark:", sayi1 - sayi2, "Çarpım:", sayi1 * sayi2, "Bölüm:", sayi1 / sayi2)
